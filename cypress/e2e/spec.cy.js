9511b6b88d9ae71dcd8aa1d048d4f613230a5037
Bernardo Domingues <bernardo.domingues@hotmart.com>
2024-04-12 09:46:05 -0300

    add neverending timeout example

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,4 +3,12 @@ describe('page', () => {
   it('works', () => {
     cy.visit('https://example.cypress.io')
   })
+  it('fails with timeout as expected', () => {
+    cy.visit('https://example.cypress.io')
+    cy.get('#this-should-fail', { timeout: 5000 })
+  })
+  it('never fails but hangs forever', () => {
+    cy.visit('https://example.cypress.io')
+    cy.get('#this-should-fail', { timeout: {} })
+  })
 })
